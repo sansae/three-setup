0d9f98314ce4b362087721b97bb94ff77ff37af6
kentsaeteurn <kentsaeteurn@gmail.com>
2024-02-25 21:38:35 -0800

    added a scene (just a black canvas/screen)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,53 @@
-import './App.css'
+import { useEffect } from "react"
+
+import * as THREE from 'three'
 
 function App() {
+  useEffect(() => {
+    const scene = new THREE.Scene()
+
+    const camera = new THREE.PerspectiveCamera(
+      50,
+      window.innerWidth / window.innerHeight,
+      1,
+      1000
+    )
+
+    camera.position.z = 96
+
+    const canvas: any = document.getElementById('myThreeJsCanvas')
+    
+    const renderer = new THREE.WebGLRenderer({
+      canvas,
+      antialias: true
+    })
+
+    renderer.setSize(window.innerWidth, window.innerHeight)
+
+    document.body.appendChild(renderer.domElement)
+
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+
+    ambientLight.castShadow = true
+    scene.add(ambientLight)
+
+    const spotLight = new THREE.SpotLight(0xffffff, 1)
+
+    spotLight.castShadow = true
+    spotLight.position.set(0, 64, 32)
+    scene.add(spotLight)
+
+    const animate = () => {
+      renderer.render(scene, camera)
+      window.requestAnimationFrame(animate)
+    }
+
+    animate()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="font-bold text-6xl">hello world</div>
-      </header>
+    <div>
+      <canvas id="myThreeJsCanvas" />
     </div>
   )
 }
