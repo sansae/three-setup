fca2d01cc92cc2c21c5ebffb19068f57818a574b
kentsaeteurn <kentsaeteurn@gmail.com>
2024-02-26 20:40:10 -0800

    added box geometry to scene

--- src/App.tsx
+++ src/App.tsx
@@ -24,20 +24,29 @@ function App() {
 
     renderer.setSize(window.innerWidth, window.innerHeight)
 
-    document.body.appendChild(renderer.domElement)
+    /** this code doesn't appear to be necessary */
+    // document.body.appendChild(renderer.domElement)
 
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+    // const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
 
-    ambientLight.castShadow = true
-    scene.add(ambientLight)
+    // ambientLight.castShadow = true
+    // scene.add(ambientLight)
 
-    const spotLight = new THREE.SpotLight(0xffffff, 1)
+    // const spotLight = new THREE.SpotLight(0xffffff, 1)
 
-    spotLight.castShadow = true
-    spotLight.position.set(0, 64, 32)
-    scene.add(spotLight)
+    // spotLight.castShadow = true
+    // spotLight.position.set(0, 64, 32)
+    // scene.add(spotLight)
+    /** this code doesn't appear to be necessary */
+
+    const boxGeometry = new THREE.BoxGeometry(16, 16, 16)
+    const boxMaterial = new THREE.MeshNormalMaterial()
+    const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial)
+    scene.add(boxMesh)
 
     const animate = () => {
+      boxMesh.rotation.x += 0.01
+      boxMesh.rotation.y += 0.01
       renderer.render(scene, camera)
       window.requestAnimationFrame(animate)
     }
