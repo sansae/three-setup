5fa87e5bf8f9ad68602c1c3724d72df31ca411b5
kentsaeteurn <kentsaeteurn@gmail.com>
2024-04-18 18:17:58 -0700

    added more textures to geometries

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,16 @@ function App() {
     const test = new SceneInit('myThreeJsCanvas');
     test.initialize();
     test.animate();
+
+    const gui = new GUI()
+
+    // add textures...
+    const spaceTexture = new THREE.TextureLoader().load("./assets/space.jpeg")
+    const uvTexture = new THREE.TextureLoader().load('./assets/uv.png')
+    const earthTexture = new THREE.TextureLoader().load('./assets/earth.jpeg')
+    const crateTexture = new THREE.TextureLoader().load('./assets/crate.png')
+    test.scene.background = spaceTexture
+
     
     // geometries
     const boxGeometry = new THREE.BoxGeometry(6, 6, 6)
@@ -24,17 +34,18 @@ function App() {
     const roundedBoxGeometry3 = new RoundedBoxGeometry(6, 6, 6, 6, 6, 6)
     
     // material that goes on geometries
-    const boxMaterial = new THREE.MeshPhongMaterial({ color: 0xffffff })
-    // const boxMaterial = new THREE.MeshStandardMaterial({ map: uvTexture })
-    
-    const boxMaterial2 = new THREE.MeshPhongMaterial({ color: 0xffffff })
+    const boxMaterial = new THREE.MeshPhongMaterial({ color: 0xffffff })    
+    // const boxMaterial2 = new THREE.MeshPhongMaterial({ color: 0xffffff })
+    const boxMaterial2 = new THREE.MeshPhongMaterial({ map: crateTexture })
     const boxMaterial3 = new THREE.MeshPhongMaterial({ color: 0xffffff })
     const roundedBoxMaterial = new THREE.MeshPhongMaterial({ 
       color: 0xffffff,
       wireframe: true,
     })
-    const roundedBoxMaterial2 = new THREE.MeshPhongMaterial({ color: 0xffffff })
-    const roundedBoxMaterial3 = new THREE.MeshPhongMaterial({ color: 0xffffff })
+    // const roundedBoxMaterial2 = new THREE.MeshPhongMaterial({ color: 0xffffff })
+    const roundedBoxMaterial2 = new THREE.MeshPhongMaterial({ map: crateTexture })
+    // const roundedBoxMaterial3 = new THREE.MeshPhongMaterial({ color: 0xffffff })
+    const roundedBoxMaterial3 = new THREE.MeshPhongMaterial({ map: earthTexture })
     
     // the mesh that brings geomtries and materials together
     const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial)
@@ -57,28 +68,11 @@ function App() {
 
 
 
-    const gui = new GUI()
-
-    // add textures
-    const spaceTexture = new THREE.TextureLoader().load("./assets/space.jpeg")
-    test.scene.background = spaceTexture
-
-    const uvTexture = new THREE.TextureLoader().load('./assets/uv.png')
-
     const bg = new THREE.BoxGeometry(6, 6, 6, 6, 6, 6)
     const bmat = new THREE.MeshStandardMaterial({ map: uvTexture })
     const bmesh = new THREE.Mesh(bg, bmat)
     bmesh.position.set(0, -8)
     test.scene.add(bmesh)
-
-    // geometry
-    // material
-    // mesh = geometry and material
-    // const cylinderGeometry = new THREE.CylinderGeometry(5, 5, 10, 32, 16);
-    // const cylinderMaterial = new THREE.MeshNormalMaterial({ wireframe: true });
-    // const cylinderMesh = new THREE.Mesh(cylinderGeometry, cylinderMaterial);
-    // cylinderMesh.position.x = 10
-    // test.scene.add(cylinderMesh)
   }, []);
 
   return (
